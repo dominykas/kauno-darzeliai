d7d9829f29a3de80146c1d9041d3f84865e0247d
Dominykas Blyžė <hello@dominykas.com>
2016-06-28 23:13:53 +0300

    2015 is already accounted for in the queues; status has bugs - consider worst case

--- vietos.js
+++ vietos.js
@@ -4,9 +4,7 @@ var _ = require('lodash');
 var data = require('./out/data.json');
 
 var eilFilter = (e) => {
-	return e.statusas !== 'Nepavyko susisiekti prašyme nurodytais kontaktais'
-		&& e.statusas !== 'Lanko (-ys) kitoje įstaigoje'
-		&& (e.mm === '2015-2016' || e.mm === '2016-2017' || e.mm === '2017-2018');
+	return (e.mm === '2015-2016' || e.mm === '2016-2017' || e.mm === '2017-2018');
 };
 
 require('fs').writeFileSync('./out/vietos.csv',
@@ -14,7 +12,6 @@ require('fs').writeFileSync('./out/vietos.csv',
 		x.det.pavadinimas,
 		x.det.seniunija,
 		encodeURIComponent(x.det.adresas.trim()),
-		_(x.grp['2015']).filter(g => g.amzius === 'priešmokyklinė').sumBy('vietos'),
 		_(x.grp['2016']).filter(g => g.amzius === 'priešmokyklinė').sumBy('vietos'),
 		_.filter(x.eil, eilFilter).length
 	].join('\t')).value().join('\n'));
